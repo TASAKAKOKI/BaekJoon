d580ae8876594fe7a98749c284bcf45799355ad8
KOKI <agiks21@gmail.com>
2020-10-13 00:38:09 +0900

    응용구현3강의준비

---
+++ SW_Expert_Academy/강의/Programming Advanced/응용_구현3_탐욕 알고리즘/D-3_5201_컨테이너 운반.py
@@ -0,0 +1,7 @@
+'''
+'''
+import  sys
+sys.stdin = open("./SW_Expert_Academy/강의/Programming Advanced/응용_구현3_탐욕 알고리즘/5201_컨테이너 운반.txt", "r")
+for tc in range(1,int(sys.stdin.readline())+1):
+    N = int(sys.stdin.readline())
+    print('#%d'%tc)
---
+++ SW_Expert_Academy/강의/Programming Advanced/응용_구현3_탐욕 알고리즘/D-3_5202_화물 도크.py
@@ -0,0 +1,7 @@
+'''
+'''
+import  sys
+sys.stdin = open("./SW_Expert_Academy/강의/Programming Advanced/응용_구현3_탐욕 알고리즘/5202_화물 도크.txt", "r")
+for tc in range(1,int(sys.stdin.readline())+1):
+    N = int(sys.stdin.readline())
+    print('#%d'%tc)
